8895157015ebba40ba0b8ff5c01436c98dae779f
saeyeonKim <118176015+saeyeonKim@users.noreply.github.com>
2023-10-26 01:52:36 +0900

    first commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div className='logoarea'>
+          <div className='logo'>SAEYEON'S <br />PORTFOLIO</div>
+        </div>
+        <div className='contentarea'>
+        <div>PROFILE</div>
+        <div>PROJECT</div>
+        <div>SKILL</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
